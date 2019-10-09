65df8165daa6a6cf0a278f47cfa84ee06e8c1a5d
s487316 <tobi.adebayo@ocsbstudent.ca>
2019-10-09 13:10:17 -0400

    push

--- ChangingAreAndPerimeterTobi/ChangingAreAndPerimeterTobi/ChangingAreaAndPerimeterForm.Designer.cs
+++ ChangingAreAndPerimeterTobi/ChangingAreAndPerimeterTobi/ChangingAreaAndPerimeterForm.Designer.cs
@@ -28,20 +28,168 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.mnuFile = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblLength = new System.Windows.Forms.Label();
+            this.lblWidth = new System.Windows.Forms.Label();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblPerimeter = new System.Windows.Forms.Label();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.lblPerimterAnswer = new System.Windows.Forms.Label();
+            this.lblWidthAnswer = new System.Windows.Forms.Label();
+            this.mnuFile.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.mnuFile.Location = new System.Drawing.Point(0, 0);
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(680, 24);
+            this.mnuFile.TabIndex = 0;
+            this.mnuFile.Text = "menuStrip1";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(37, 20);
+            this.mniFile.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(180, 22);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.MniExit_Click);
+            // 
+            // lblLength
+            // 
+            this.lblLength.AutoSize = true;
+            this.lblLength.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLength.Location = new System.Drawing.Point(35, 52);
+            this.lblLength.Name = "lblLength";
+            this.lblLength.Size = new System.Drawing.Size(98, 25);
+            this.lblLength.TabIndex = 1;
+            this.lblLength.Text = "Length :";
+            // 
+            // lblWidth
+            // 
+            this.lblWidth.AutoSize = true;
+            this.lblWidth.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWidth.Location = new System.Drawing.Point(35, 128);
+            this.lblWidth.Name = "lblWidth";
+            this.lblWidth.Size = new System.Drawing.Size(86, 25);
+            this.lblWidth.TabIndex = 2;
+            this.lblWidth.Text = "Width :";
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(178, 57);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(100, 20);
+            this.txtLength.TabIndex = 3;
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(178, 128);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 20);
+            this.txtWidth.TabIndex = 4;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(310, 93);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+            // 
+            // lblPerimeter
+            // 
+            this.lblPerimeter.AutoSize = true;
+            this.lblPerimeter.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPerimeter.Location = new System.Drawing.Point(37, 261);
+            this.lblPerimeter.Name = "lblPerimeter";
+            this.lblPerimeter.Size = new System.Drawing.Size(199, 25);
+            this.lblPerimeter.TabIndex = 6;
+            this.lblPerimeter.Text = "The Perimeter is :";
+            this.lblPerimeter.Click += new System.EventHandler(this.Lbl_Click);
+            // 
+            // lblArea
+            // 
+            this.lblArea.AutoSize = true;
+            this.lblArea.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArea.Location = new System.Drawing.Point(37, 310);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(145, 25);
+            this.lblArea.TabIndex = 7;
+            this.lblArea.Text = "The area is :";
+            // 
+            // lblPerimterAnswer
+            // 
+            this.lblPerimterAnswer.AutoSize = true;
+            this.lblPerimterAnswer.Location = new System.Drawing.Point(335, 261);
+            this.lblPerimterAnswer.Name = "lblPerimterAnswer";
+            this.lblPerimterAnswer.Size = new System.Drawing.Size(35, 13);
+            this.lblPerimterAnswer.TabIndex = 8;
+            this.lblPerimterAnswer.Text = "label3";
+            // 
+            // lblWidthAnswer
+            // 
+            this.lblWidthAnswer.AutoSize = true;
+            this.lblWidthAnswer.Location = new System.Drawing.Point(341, 322);
+            this.lblWidthAnswer.Name = "lblWidthAnswer";
+            this.lblWidthAnswer.Size = new System.Drawing.Size(35, 13);
+            this.lblWidthAnswer.TabIndex = 9;
+            this.lblWidthAnswer.Text = "label4";
             // 
             // frmChangingAreaAndPerimeter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(680, 383);
+            this.Controls.Add(this.lblWidthAnswer);
+            this.Controls.Add(this.lblPerimterAnswer);
+            this.Controls.Add(this.lblArea);
+            this.Controls.Add(this.lblPerimeter);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.lblWidth);
+            this.Controls.Add(this.lblLength);
+            this.Controls.Add(this.mnuFile);
+            this.MainMenuStrip = this.mnuFile;
             this.Name = "frmChangingAreaAndPerimeter";
             this.Text = "Changing Area And Perimeter Tobi";
+            this.mnuFile.ResumeLayout(false);
+            this.mnuFile.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.Label lblLength;
+        private System.Windows.Forms.Label lblWidth;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblPerimeter;
+        private System.Windows.Forms.Label lblArea;
+        private System.Windows.Forms.Label lblPerimterAnswer;
+        private System.Windows.Forms.Label lblWidthAnswer;
     }
 }
 
--- ChangingAreAndPerimeterTobi/ChangingAreAndPerimeterTobi/ChangingAreaAndPerimeterForm.cs
+++ ChangingAreAndPerimeterTobi/ChangingAreAndPerimeterTobi/ChangingAreaAndPerimeterForm.cs
@@ -15,6 +15,27 @@ namespace ChangingAreAndPerimeterTobi
         public frmChangingAreaAndPerimeter()
         {
             InitializeComponent();
+            //Hide the perimeter and answer labels and answers when the user clicks on the calculate button
+            this.lblPerimeter.Hide();
+            this.lblPerimterAnswer.Hide();
+            this.lblWidth.Hide();
+            this.lblWidthAnswer.Hide();
+        }
+
+        private void MniExit_Click(object sender, EventArgs e)
+        {
+            //This closes the application
+            this.Close();
+        }
+
+        private void Lbl_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnCalculate_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }

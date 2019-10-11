d1eb1b3352f11a4c348e464fa4fac9f6a7dda9fc
s487316 <tobi.adebayo@ocsbstudent.ca>
2019-10-11 12:37:38 -0400

    push

---
+++ TopDowndesignAndFlowChartTobi/TopDowndesignAndFlowChartTobi/TopDownDesignAndFlowChartForm.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace TopDowndesignAndFlowChartTobi
+{
+    partial class TopDownDesignAndFlowChartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mnuFile = new System.Windows.Forms.MenuStrip();
+            this.mniFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblEnterRaidus = new System.Windows.Forms.Label();
+            this.lblCircumferenceIs = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.mnuFile.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.mnuFile.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniFile});
+            this.mnuFile.Location = new System.Drawing.Point(0, 0);
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(390, 28);
+            this.mnuFile.TabIndex = 0;
+            this.mnuFile.Text = "menuStrip1";
+            // 
+            // mniFile
+            // 
+            this.mniFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniExit});
+            this.mniFile.Name = "mniFile";
+            this.mniFile.Size = new System.Drawing.Size(46, 24);
+            this.mniFile.Text = "File";
+            // 
+            // mniExit
+            // 
+            this.mniExit.Name = "mniExit";
+            this.mniExit.Size = new System.Drawing.Size(224, 26);
+            this.mniExit.Text = "Exit";
+            this.mniExit.Click += new System.EventHandler(this.mniExit_Click);
+            // 
+            // lblEnterRaidus
+            // 
+            this.lblEnterRaidus.AutoSize = true;
+            this.lblEnterRaidus.Location = new System.Drawing.Point(12, 79);
+            this.lblEnterRaidus.Name = "lblEnterRaidus";
+            this.lblEnterRaidus.Size = new System.Drawing.Size(237, 17);
+            this.lblEnterRaidus.TabIndex = 1;
+            this.lblEnterRaidus.Text = "Enter the radius of the circle (in cm):";
+            // 
+            // lblCircumferenceIs
+            // 
+            this.lblCircumferenceIs.AutoSize = true;
+            this.lblCircumferenceIs.Location = new System.Drawing.Point(24, 214);
+            this.lblCircumferenceIs.Name = "lblCircumferenceIs";
+            this.lblCircumferenceIs.Size = new System.Drawing.Size(148, 17);
+            this.lblCircumferenceIs.TabIndex = 2;
+            this.lblCircumferenceIs.Text = "The circumference is :";
+            this.lblCircumferenceIs.Click += new System.EventHandler(this.lblCircumferenceIs_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Location = new System.Drawing.Point(275, 214);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(46, 17);
+            this.lblAnswer.TabIndex = 3;
+            this.lblAnswer.Text = "label3";
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Location = new System.Drawing.Point(278, 79);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(100, 22);
+            this.txtRadius.TabIndex = 4;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(289, 128);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // TopDownDesignAndFlowChartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 283);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.txtRadius);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lblCircumferenceIs);
+            this.Controls.Add(this.lblEnterRaidus);
+            this.Controls.Add(this.mnuFile);
+            this.MainMenuStrip = this.mnuFile;
+            this.Name = "TopDownDesignAndFlowChartForm";
+            this.Text = "Form1";
+            this.mnuFile.ResumeLayout(false);
+            this.mnuFile.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mniFile;
+        private System.Windows.Forms.ToolStripMenuItem mniExit;
+        private System.Windows.Forms.Label lblEnterRaidus;
+        private System.Windows.Forms.Label lblCircumferenceIs;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ TopDowndesignAndFlowChartTobi/TopDowndesignAndFlowChartTobi/TopDownDesignAndFlowChartForm.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * Created by: Tobi Adebayo
+ * Created on: 11-10-2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #10 - TopDownDesignAndFlowChart
+ * This program displays the circumference of a circle when given the radius
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TopDowndesignAndFlowChartTobi
+{
+    public partial class TopDownDesignAndFlowChartForm : Form
+    {
+        public TopDownDesignAndFlowChartForm()
+        {
+            InitializeComponent();
+            //Hide the 2 labels
+            this.lblCircumferenceIs.Hide();
+            this.lblAnswer.Hide();
+        }
+
+        private void mniExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void lblCircumferenceIs_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //Display the 2 hidden labels
+            this.lblCircumferenceIs.Show();
+            this.lblAnswer.Show();
+
+            //declear constant
+            const double PI = 3.14;
+
+            //Declear variable
+            double radius, circumference;
+
+            //convert circumference to string
+            radius = double.Parse(txtRadius.Text);
+
+            //calculate the radius of the circle
+            circumference = 2 * 3.4 * radius;
+            this.lblAnswer.Text = Convert.ToString(circumference) + "cm";
+
+        }
+    }
+}
